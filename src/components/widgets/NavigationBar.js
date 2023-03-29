29522e5a41db837bbf2e182d863efeb57f1c0dcb
enochviewu95 <viewuenoch@gmail.com>
2023-03-29 11:47:47 +0000

    Added features to homepage, created contact-us, about-us pages

--- src/components/widgets/NavigationBar.js
+++ src/components/widgets/NavigationBar.js
@@ -3,12 +3,13 @@ import { useState } from 'react'
 
 
 import { Dialog } from '@headlessui/react'
+import { Link } from 'react-router-dom'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-    { name: 'Home', href: '/' },
-    { name: 'About Us', href: '/about-us' },
-    { name: 'Contact Us', href: '/contact-us' },
+    { name: 'Home', href: '/fashion-shop-fe' },
+    { name: 'About Us', href: '/fashion-shop-fe/about-us' },
+    { name: 'Contact Us', href: '/fashion-shop-fe/contact-us' },
 ]
 
 export default function NavigationBar() {
@@ -19,14 +20,14 @@ export default function NavigationBar() {
         <header className="absolute inset-x-0 top-0 z-50">
             <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="/" className="-m-1.5 p-1.5">
+                    <Link to="/fashion-shop-fe" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
                         <img
                             className="h-8 w-auto"
                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt=""
                         />
-                    </a>
+                    </Link>
                 </div>
                 <div className="flex lg:hidden">
                     <button
@@ -40,29 +41,29 @@ export default function NavigationBar() {
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link key={item.name} to={item.href} className="text-sm font-semibold leading-6 text-gray-900">
                             {item.name}
-                        </a>
+                        </Link>
                     ))}
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="/register" className="text-sm font-semibold leading-6 text-gray-900">
+                    <Link to="/fashion-shop-fe/register" className="text-sm font-semibold leading-6 text-gray-900">
                         Log in <span aria-hidden="true">&rarr;</span>
-                    </a>
+                    </Link>
                 </div>
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-50" />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="/" className="-m-1.5 p-1.5">
+                        <Link to="/fashion-shop-fe" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                 alt=""
                             />
-                        </a>
+                        </Link>
                         <button
                             type="button"
                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -76,22 +77,22 @@ export default function NavigationBar() {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
-                                    <a
+                                    <Link
                                         key={item.name}
-                                        href={item.href}
+                                        to={item.href}
                                         className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                     >
                                         {item.name}
-                                    </a>
+                                    </Link>
                                 ))}
                             </div>
                             <div className="py-6">
-                                <a
-                                    href="/register"
+                                <Link
+                                    to="/fashion-shop-fe/register"
                                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Log in
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Client from './routes/Client';
@@ -12,43 +12,49 @@ import Registration from './routes/Registration';
 
 
 export const Routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <PageNotFound />,
-    children: [
-      {
-        path: '/',
-        element: <Client />,
-        children: [
-          {
-            path: '/',
-            element: <HomepageView />,
-          },
-          {
-            path: '/about-us',
-            element: <AboutUsView />
-          },
-          {
-            path: '/contact-us',
-            element: <ContactUsView />
-          },
-          {
-            path: '/fashion-shop-fe',
-            element:<HomepageView/>
-          }
-        ]
-      },
-      {
-        path: '/admin',
-        element: <Admin />,
-        errorElement: <PageNotFound />
-      },
-      {
-        path: '/register',
-        element: <Registration />,
-        errorElement: <PageNotFound />
-      }
-    ]
-  }
-])
+    {
+      path:'/',
+      element: <App/>,
+      errorElement: <PageNotFound/>,
+      children:[
+        {
+          path:'/',
+          element: <Client/>,
+          children:[
+            {
+              path:'/',
+              element:<HomepageView/>
+            }
+          ]
+        },
+        {
+          path:'/fashion-shop-fe',
+          element: <Client/>,
+          children:[
+            {
+              path: '/fashion-shop-fe',
+              element:<HomepageView/>,
+            },
+            {
+              path: '/fashion-shop-fe/about-us',
+              element:<AboutUsView/>
+            },
+            {
+              path:'/fashion-shop-fe/contact-us',
+              element: <ContactUsView/>
+            }
+          ]
+        },
+        {
+          path:'/admin',
+          element:<Admin/>,
+          errorElement: <PageNotFound/>
+        },
+        {
+            path:'/fashion-shop-fe/register',
+            element: <Registration/>,
+            errorElement: <PageNotFound/>
+        }
+      ]
+    }
+  ])
