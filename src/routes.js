6eb7896b75c79037e8d26d51c6bf44abf58a42e8
enochviewu95 <viewuenoch@gmail.com>
2023-03-29 10:34:09 +0000

    Added features to homepage, created contact-us, about-us pages

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 
 import App from './App';
 import Client from './routes/Client';
@@ -12,39 +12,43 @@ import Registration from './routes/Registration';
 
 
 export const Routes = createBrowserRouter([
-    {
-      path:'/',
-      element: <App/>,
-      errorElement: <PageNotFound/>,
-      children:[
-        {
-          path:'/',
-          element: <Client/>,
-          children:[
-            {
-              path: '/',
-              element:<HomepageView/>,
-            },
-            {
-              path: '/about-us',
-              element:<AboutUsView/>
-            },
-            {
-              path:'/contact-us',
-              element: <ContactUsView/>
-            },
-          ]
-        },
-        {
-          path:'/admin',
-          element:<Admin/>,
-          errorElement: <PageNotFound/>
-        },
-        {
-            path:'/register',
-            element: <Registration/>,
-            errorElement: <PageNotFound/>
-        }
-      ]
-    }
-  ])
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Client />,
+        children: [
+          {
+            path: '/',
+            element: <HomepageView />,
+          },
+          {
+            path: '/about-us',
+            element: <AboutUsView />
+          },
+          {
+            path: '/contact-us',
+            element: <ContactUsView />
+          },
+          {
+            path: '/fashion-shop-fe',
+            element:<HomepageView/>
+          }
+        ]
+      },
+      {
+        path: '/admin',
+        element: <Admin />,
+        errorElement: <PageNotFound />
+      },
+      {
+        path: '/register',
+        element: <Registration />,
+        errorElement: <PageNotFound />
+      }
+    ]
+  }
+])
