70adb2224ea723b495fa8fef1be0a6d334832004
enochviewu95 <viewuenoch@gmail.com>
2023-04-07 19:13:16 +0000

    Added slide over to admin dashboard

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -3,7 +3,7 @@ import NavigationBar from '../components/widgets/NavigationBar'
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
-import { Link, NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default function Admin() {
 
